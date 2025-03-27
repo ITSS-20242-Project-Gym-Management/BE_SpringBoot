41d9223a3bd9f05b01847e4be7f1de7aa3c90910
ディン。ディン。ロック <163549934+Roku2004@users.noreply.github.com>
2025-03-27 15:19:39 +0700

    Add files via upload

---
+++ BE_SpringBoot/src/main/java/com/example/itssprj_ver1/model/payment.java
@@ -0,0 +1,33 @@
+package com.example.itssprj_ver1.model;
+
+import jakarta.persistence.*;
+
+import java.sql.Date;
+
+@Entity
+@Table(name="payment")
+public class payment {
+
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.AUTO)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "customerid", nullable = false)
+    private customer customer;
+
+    @Column(name = "amount",nullable = false)
+    private Float amount;
+
+    @Column(name = "method",nullable = false)
+    private String method;
+
+    @Column(name = "paid",nullable = false)
+    private Boolean paid;
+
+    @Column(name = "createAt",nullable = false)
+    private Date createAt;
+
+
+
+}
