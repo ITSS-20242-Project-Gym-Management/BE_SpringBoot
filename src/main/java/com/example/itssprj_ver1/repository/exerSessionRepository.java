c3fd4356c356221a4fb96bd58c33c73f711a3657
ディン。ディン。ロック <163549934+Roku2004@users.noreply.github.com>
2025-05-03 23:29:46 +0700

    Update exerSessionRepository.java

--- src/main/java/com/example/itssprj_ver1/repository/exerSessionRepository.java
+++ src/main/java/com/example/itssprj_ver1/repository/exerSessionRepository.java
@@ -2,8 +2,12 @@ package com.example.itssprj_ver1.repository;
 
 import com.example.itssprj_ver1.model.exerciseSession;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface exerSessionRepository extends JpaRepository<exerciseSession, Integer> {
+    List<exerciseSession> findByStaff_Userid_Role_Roleid(Integer roleId);
 }
