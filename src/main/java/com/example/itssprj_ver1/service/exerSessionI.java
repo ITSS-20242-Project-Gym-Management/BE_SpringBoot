4ae179c144f437a2254804907f8cc054e05c56e6
LongPham <longpx1508@gmail.com>
2025-05-06 21:46:14 +0700

    update pt usecase

--- src/main/java/com/example/itssprj_ver1/service/exerSessionI.java
+++ src/main/java/com/example/itssprj_ver1/service/exerSessionI.java
@@ -2,24 +2,29 @@ package com.example.itssprj_ver1.service;
 
 import com.example.itssprj_ver1.model.exerciseSession;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+import org.springframework.http.ResponseEntity;
+
 public interface exerSessionI {
 
      List<Map<String,Object>> getAllSessions();
-    boolean addSession(String cufirstname,String culastname, String ptfirstname,String ptlastname, String exerciseType);
-    boolean updateSession(int sessionid,String cufirstname, String culastname, String ptfirstname, String ptlastname, String exerciseType);
+     boolean addSession(String cufirstname,String culastname, String ptfirstname,String ptlastname, String exerciseType);
+     boolean updateSession(int sessionid,String cufirstname, String culastname, String ptfirstname, String ptlastname, String exerciseType);
      
      List<exerciseSession> getAllSession();
      Optional<exerciseSession> getSessionById(Integer id);
      List<exerciseSession> getSessionByCustomerId(Integer customerId);
      List<exerciseSession> getSessionsByTrainerId(Integer trainerId);
 
-     exerciseSession createSession(exerciseSession newSession);
-     exerciseSession updateSession(Integer id, exerciseSession updatedSession);
-
+     ResponseEntity<?> createSession(Integer trainerId, Integer customerId, String exerciseType,
+                                     LocalDateTime beginAt, LocalDateTime endAt, String description);
+     ResponseEntity<?> updateSession(Integer sessionId, Integer trainerId, Integer customerId,
+                                    String exerciseType, LocalDateTime beginAt, LocalDateTime endAt,
+                                    String description);
      void deleteSession(Integer id);
 
 
--- src/main/java/com/example/itssprj_ver1/service/ptServiceI.java
+++ src/main/java/com/example/itssprj_ver1/service/ptServiceI.java
@@ -4,7 +4,11 @@ package com.example.itssprj_ver1.service;
 import java.util.List;
 import java.util.Map;
 
+import com.example.itssprj_ver1.model.customer;
+
 public interface ptServiceI {
     boolean addPT(String firstname, String lastname, String email, String phone, String gender, int age, int userid);
     List<Map<String,Object>> findAll();
+    List<customer> customerListByTrainer(Integer trainerId);
+
 }
